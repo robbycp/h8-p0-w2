967b02ea3cb2212beb6326be87f27313caaf67be
robbycp <robbycaesar@gmail.com>
2019-03-27 17:20:50 +0700

    additional fix

--- exercise-6.js
+++ exercise-6.js
@@ -4,13 +4,13 @@ console.log("1. Melakukan Looping Menggunakan While");
 console.log("LOOPING PERTAMA");
 var x = 0;
 while(x < 20) {
-    x = x + 2;
+    x += 2;
     console.log(x + " - I love coding");
 }
 console.log("LOOPING KEDUA")
 while(x >= 2) {
     console.log(x + " - I will become fullstack developer");
-    x = x - 2;
+    x -= 2;
 }
 
 //2.Melakukan Looping Menggunakan For
@@ -31,38 +31,34 @@ console.log("a. Ganjil Genap");
 for(var x = 1; x <= 100; x++) {
     if (x % 2 == 0) {
         console.log("GENAP");
-    }
-    else {
+    } else {
         console.log("GANJIL");
     }
 }
 //b. pertambahan counter 2, kelipatan 3
 console.log("b. pertambahan counter 2, kelipatan 3");
-for(var x = 1; x <= 100; x = x + 2) {
+for(var x = 1; x <= 100; x += 2) {
     if (x % 3 == 0) {
         console.log(x + " KELIPATAN 3");
-    }
-    else {
+    } else {
         console.log("");
     }
 }
 //c. pertambahan counter 5, kelipatan 6
 console.log("c. pertambahan counter 5, kelipatan 6");
-for(var x = 1; x <= 100; x = x + 5) {
+for(var x = 1; x <= 100; x += 5) {
     if (x % 6 == 0) {
         console.log(x + " KELIPATAN 6");
-    }
-    else {
+    } else {
         console.log("");
     }
 }
 //d. pertambahan counter 9, kelipatan 10
 console.log("d. pertambahan counter 9, kelipatan 10");
-for(var x = 1; x <= 100; x = x + 9) {
+for(var x = 1; x <= 100; x += 9) {
     if (x % 10 == 0) {
         console.log(x + " KELIPATAN 10");
-    }
-    else {
+    } else {
         console.log("");
     }
 }
